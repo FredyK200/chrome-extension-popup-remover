fc8ea89a3ecdafc16f4788006f3bdad9964fbab8
FredyK200 <qwertygeek01@gmail.com>
2021-04-27 09:45:31 -0400

    Initial Concept
    Only tested on Facebook because I can't think of any websites to test.

--- background.js
+++ background.js
@@ -1,10 +1,31 @@
-function reddenPage() {
-    document.body.style.backgroundColor = 'red';
+function removePopup() {
+    var elems = document.getElementsByTagName('*');
+    var highest = Number.MIN_SAFE_INTEGER || -(Math.pow(2, 53) - 1);
+    for (var i = 0; i < elems.length; i++) {
+        var zindex = Number.parseInt(
+            document.defaultView.getComputedStyle(elems[i], null).getPropertyValue("z-index"),
+            10
+        );
+        if (zindex > highest) {
+            highest = zindex;
+        }
+    }
+    for (var i = 0; i < elems.length; i++) {
+        var zindex = Number.parseInt(
+            document.defaultView.getComputedStyle(elems[i], null).getPropertyValue("z-index"),
+            10
+        );
+        if (zindex == highest) {
+            elems[i].remove();
+        }
+    }
+    document.body.style.overflowX = null;
+    document.body.style.overflowY = scroll;
 }
 
 chrome.action.onClicked.addListener((tab) => {
     chrome.scripting.executeScript({
         target: { tabId: tab.id },
-        function: reddenPage
+        function: removePopup
     });
 });
