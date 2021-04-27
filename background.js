41e94337c8738d3ea41399ed7b4c9f777a9e2b80
FredyK200 <qwertygeek01@gmail.com>
2021-04-27 08:53:57 -0400

    Different Sample

--- background.js
+++ background.js
@@ -1,6 +1,10 @@
-let color = '#3aa757';
+function reddenPage() {
+    document.body.style.backgroundColor = 'red';
+}
 
-chrome.runtime.onInstalled.addListener(() => {
-    chrome.storage.sync.set({ color });
-    console.log('Default background color set to %cgreen', `color: ${color}`);
+chrome.action.onClicked.addListener((tab) => {
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: reddenPage
+    });
 });
